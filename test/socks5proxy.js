b6c567c06523b41f57c96e62564715c6fc5caa76
Jan Zhen <jan365@gmail.com>
2013-05-05 15:20:15 +0800

    Relay stream after connected to remote server.

--- test/socks5proxy.js
+++ test/socks5proxy.js
@@ -8,30 +8,32 @@ var proxyServer = socks5.createSocks5Server(function(clnSocket, rPort, rHost){
     // after proxy hanshake
     var remote = net.connect(rPort, rHost, function(){
         // connected, send ready to client
+        relay(clnSocket, remote);
         socks5.proxyReady(clnSocket);
     });
 
-    // relay
-    remote.on('data', function(chunk){
-        clnSocket.write(chunk);
-    });
-    clnSocket.on('data', function(chunk){
-        remote.write(chunk);
-    });
-    remote.on('close', function(err){
-        clnSocket.end();
-        console.log('Remote closed.');
-    });
-    clnSocket.on('close', function(err){
-        if(remote){
-            remote.end();
-        }
-        console.log('Client closed.');
-    });
 });
 
+function relay(cSocket, rSocket){
+    // relay
+    rSocket.on('data', function(chunk){
+        cSocket.write(chunk);
+    });
+    cSocket.on('data', function(chunk){
+        rSocket.write(chunk);
+    });
+    rSocket.on('close', function(err){
+        cSocket.end();
+        console.log('Remote closed.');
+    });
+    cSocket.on('close', function(err){
+        rSocket.end();
+        console.log('Client closed.');
+    });
+}
+
 proxyServer.on('error', function(e){
-    console.error('Server error: %j.' , e);
+    console.error('Server error: %j.', e);
 });
 
 proxyServer.listen(PORT, HOST);
